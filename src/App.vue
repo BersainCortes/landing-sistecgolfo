c1135c4cc2084162c503901c04c245e23c4b63d4
BersainCortes <bersaincortes10@gmail.com>
2024-04-03 12:53:26 -0600

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container-fluid">
+    <NavBar></NavBar>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style src="./assets/bootstrap/css/bootstrap.min.css"></style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid">
+      <!-- Logotipo -->
+      <a class="navbar-brand" href="#">Logo</a>
+
+      <!-- Botón de hamburguesa para pantallas pequeñas -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Contenedor de elementos de la barra de navegación -->
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style src="../assets/css/NavBar.css" scoped></style>
